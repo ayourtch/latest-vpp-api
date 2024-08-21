3641f7bc3d18c65cd8437999ec1a5e430ad5bbcf
Andrew Yourtchenko (ghjobs) <ayourtch+ghjobs@gmail.com>
2024-08-21 04:47:00 +0000

    Auto-update at Wed Aug 21 04:47:00 UTC 2024

--- src/http_static.rs
+++ src/http_static.rs
@@ -28,3 +28,22 @@ pub struct HttpStaticEnableReply {
 	pub context: u32,
 	pub retval: i32,
 }
+#[derive(Debug, Clone, Serialize, Deserialize, VppMessage)]
+#[message_name_and_crc(http_static_enable_v2_3ee5f1f6)]
+pub struct HttpStaticEnableV2 {
+	pub client_index: u32,
+	pub context: u32,
+	pub fifo_size: u32,
+	pub cache_size_limit: u32,
+	pub max_age: u32,
+	pub prealloc_fifos: u32,
+	pub private_segment_size: u32,
+	pub www_root: FixedSizeString<typenum::U256>,
+	pub uri: FixedSizeString<typenum::U256>,
+}
+#[derive(Debug, Clone, Serialize, Deserialize, VppMessage)]
+#[message_name_and_crc(http_static_enable_v2_reply_e8d4e804)]
+pub struct HttpStaticEnableV2Reply {
+	pub context: u32,
+	pub retval: i32,
+}
